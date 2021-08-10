c45bab8ee0ccf3e2129099df3330fa1c5be602d8
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-10 13:38:56 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,30 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Информатика",
+        options:["Information Technology", "Physical Education", "school subjects", "pencil case"],
+        answer: 0
+    },
+    {
+        q:"Физкультура",
+        options:["lunch break", "Physical Education", "Information Technology", "school subjects"],
+        answer: 1
+    },
+    {
+        q:"обеденный перерыв",
+        options:["Physical Education", "school subjects", "lunch break", "pencil case"],
+        answer: 2
+    },
+    {
+        q:"пенал",
+        options:["lunch break", "Information Technology", "school subjects", "pencil case"],
+        answer: 3
+    },
+    {
+        q:"школьные предметы",
+        options:["school subjects", "pencil case", "Information Technology", "lunch break"],
+        answer: 0
+    }
+
+]
